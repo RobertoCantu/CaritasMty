00aff94c51a50b21b886672cd2972b06fbc0f0cf
Roberto <roberto.cantu@gmail.com>
2021-11-07 19:24:18 -0600

    Added port to config file

--- tectrade/server/src/index.js
+++ tectrade/server/src/index.js
@@ -1,10 +1,7 @@
 import usersRoutes from '../src/routes/usersroutes.js';
 import ticketsRoutes from '../src/routes/ticketsroutes';
 import app from './app.js';
-
-
-//Process env Port process.env.Port 
-const port = process.env.PORT || 3001;
+import config from './config'
 
 //Admin users
 const admin = ["juan@caritas.com","lorena@caritas.com","gerardo@caritas.com","sara@caritas.com"];
@@ -15,6 +12,6 @@ app.use(usersRoutes);
 app.use(ticketsRoutes);
 
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`);
 })
