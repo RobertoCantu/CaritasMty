5e21db52655707f7d6447fa3622fabec815ef2f4
Roberto <roberto.cantu@gmail.com>
2021-11-09 11:22:27 -0600

    Aded date validation in Ticket form

--- tectrade/client/src/Validations/TicketValidation.js
+++ tectrade/client/src/Validations/TicketValidation.js
@@ -1,8 +1,8 @@
 import * as yup from 'yup'
 
 export const ticketSchema = yup.object().shape({
-    title: yup.string().required("Ticket title is required"),
-    department: yup.string().required("Select a department"),
-    description: yup.string().required("Enter a description"),
-    date: yup.date("Select date").required()
+    title: yup.string().required("El titulo del ticket es requerido"),
+    department: yup.string().required("Selecciona un departamento"),
+    description: yup.string().required("Ingresa una descripcion"),
+    date: yup.date().min(new Date(),"Aun no podemos viajar al pasado").max(new Date(), "Aun no podemos viajar al futuro").typeError('Seleciona el dia de hoy')
 });
