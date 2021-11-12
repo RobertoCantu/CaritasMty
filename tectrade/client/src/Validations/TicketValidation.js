9393f0d7b390e4564fb769196ee506d8fd68d44f
Enrique Elizondo <quiqueev@gmail.com>
2021-11-12 11:19:38 -0600

    Ticket delete

--- tectrade/client/src/Validations/TicketValidation.js
+++ tectrade/client/src/Validations/TicketValidation.js
@@ -1,8 +1,16 @@
-import * as yup from 'yup'
+import * as yup from "yup";
+
+// Used to fix bug.
+const today = new Date();
+today.setHours(0, 0, 0, 0);
 
 export const ticketSchema = yup.object().shape({
-    title: yup.string().required("El titulo del ticket es requerido"),
-    department: yup.string().required("Selecciona un departamento"),
-    description: yup.string().required("Ingresa una descripcion"),
-    date: yup.date().min(new Date(),"Aun no podemos viajar al pasado").max(new Date(), "Aun no podemos viajar al futuro").typeError('Seleciona el dia de hoy')
+  title: yup.string().required("El titulo del ticket es requerido"),
+  department: yup.string().required("Selecciona un departamento"),
+  description: yup.string().required("Ingresa una descripcion"),
+  date: yup
+    .date()
+    .min(today, "Aun no podemos viajar al pasado")
+    .max(new Date(), "Aun no podemos viajar al futuro")
+    .typeError("Seleciona el dia de hoy")
 });
--- tectrade/client/src/components/EmployeeTickets/EmployeeTickets.js
+++ tectrade/client/src/components/EmployeeTickets/EmployeeTickets.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useContext} from 'react'
+import React, { useEffect, useContext } from 'react'
 import Axios from 'axios'
 import { UserContext } from '../../Helper/Context';
 import { Link } from 'react-router-dom';
@@ -6,48 +6,69 @@ import { Link } from 'react-router-dom';
 
 function EmployeeTickets() {
   //Context api
-  const {user,setUser} = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
 
   //Obtain tickets information
   useEffect(() => {
-  Axios.get(`http://localhost:3001/${user.id}/tickets`).
-  then((res) => {
-    setUser({...user,tickets:res.data});
-  }).catch(error => {
-      console.log(error);
-  })
+    Axios.get(`http://localhost:3001/${user.id}/tickets`).
+      then((res) => {
+        setUser({ ...user, tickets: res.data });
+      }).catch(error => {
+        console.log(error);
+      })
   }, []);
 
   console.log(user);
 
-   
- 
-    return (
-        <div >
-            <h1>My tickets</h1>
-            <div className="container row">
-            {user.tickets.length ? user.tickets.map(ticket => (
-                <div key={ticket.TicketId} className="card m-3"  style={{width: '18rem'}}>
-                <img className="card-img-top" src="..." alt="Card image cap"/>
-                <div className="card-body">
-                  <Link to={`tickets/${ticket.TicketId}`}><h5 className="card-title">{ticket.Title}</h5></Link>
-                  <p className="card-text">{ticket.Description}</p>
-                  <a href="#" className="btn btn-info">Edit</a>
-                  <a href="#"  className="btn btn-danger">Delete</a>
-                </div>
-              </div>
-            )) 
-            :
-            <div>
+  function deleteClick(ticket) {
+    Axios.delete(`http://localhost:3001/deleteTicket/${ticket.TicketId}`)
+      .then((res) => {
+        console.log(res.data.messageSuccess);
+        Axios.get(`http://localhost:3001/${user.id}/tickets`).
+          then((res) => {
+            setUser({ ...user, tickets: res.data });
+          }).catch(error => {
+            console.log(error);
+          })
+
+      }).catch(err => {
+        console.log(err);
+      })
+    // console.log(ticket.TicketId);
+  }
+
+  function editClick(ticket) {
+    console.log(ticket);
+  }
+
+
+
+  return (
+    <div >
+      <h1>My tickets</h1>
+      <div className="container row">
+        {user.tickets.length ? user.tickets.map(ticket => (
+          <div key={ticket.TicketId} className="card m-3" style={{ width: '18rem' }}>
+            <img className="card-img-top" src="..." alt="Card image cap" />
+            <div className="card-body">
+              <Link to={`tickets/${ticket.TicketId}`}><h5 className="card-title">{ticket.Title}</h5></Link>
+              <p className="card-text">{ticket.Description}</p>
+              <a href="#" className="btn btn-info" onClick={() => editClick(ticket)}>Edit</a>
+              <a href="#" className="btn btn-danger" onClick={() => deleteClick(ticket)}>Delete</a>
+            </div>
+          </div>
+        ))
+          :
+          <div>
             <h1>Hey {user.firstName} you haven't create any Ticket</h1>
             <h2>If you have a problem please create a Ticket on this button</h2>
             <Link to={'/create'}><button type="button" className="btn btn-success">Ticket</button></Link>
-            </div>
-            }
-            </div>
-            
-        </div>
-    )
+          </div>
+        }
+      </div>
+
+    </div>
+  )
 }
 
 export default EmployeeTickets
