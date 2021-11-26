d95c4937f50fb6559dfdaf83f7a099179d7626c9
DoggedFilly0 <A01366287@itesm.mx>
2021-11-25 19:12:05 -0600

    ligeros ajustes a tickets

--- tectrade/client/src/components/TicketDetails/TicketDetails.js
+++ tectrade/client/src/components/TicketDetails/TicketDetails.js
@@ -1,24 +1,30 @@
-import React, { useContext,useState,useEffect } from 'react'
-import { useParams } from 'react-router'
-import { UserContext } from '../../Helper/Context';
-
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { UserContext } from "../../Helper/Context";
 
 function TicketDetails() {
-    const {user} = useContext(UserContext);
-    const {id} = useParams();
-    const [result,setResult] = useState([]);
-    
-    
-    //This hook obtains a ticked based on url id 
-    useEffect(()=>{
-        setResult(user.tickets.filter(ticket => ticket.TicketId === parseInt(id)));
-    },[user,id])
+  const { user } = useContext(UserContext);
+  const { id } = useParams();
+  const [result, setResult] = useState([]);
 
-    return (
-        <div>
-            {result.length ? <h1>{result[0].Title}</h1>: <h1>Lo sentimos {user.firstName}, no existe Ticket con el id: {id}</h1>}
-        </div>
-    )
+  //This hook obtains a ticked based on url id
+  useEffect(() => {
+    setResult(
+      user.tickets.filter((ticket) => ticket.TicketId === parseInt(id))
+    );
+  }, [user, id]);
+
+  return (
+    <div className="container col-12">
+      {result.length ? (
+        <h1>{result[0].Title}</h1>
+      ) : (
+        <h1 className="container row justify-content-center">
+          Lo sentimos {user.firstName}, no existe Ticket con el id: {id}
+        </h1>
+      )}
+    </div>
+  );
 }
 
-export default TicketDetails
+export default TicketDetails;
